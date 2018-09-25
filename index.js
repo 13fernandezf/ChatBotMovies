64dc9ba8bd6ff31fd1da575d658987badb9292db
fernandez <florian.fernandez@capgemini.com>
2018-09-25 10:43:02 +0200

    8eme  commit

--- index.js
+++ index.js
@@ -4,52 +4,97 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('https');
 var unirest = require("unirest");
-let errorResposne = {
-    results: []
-};
 var port = process.env.PORT || 8080;
 // create serve and configure it.
 const server = express();
 server.use(bodyParser.json());
-server.post('/getMovies',function (req,res)  {
-    var movies = req.body.queryResult.parameters.movies_name;
-    if(movies){
-      var response = "This is a sample response from your webhook!";//Default response from the webhook to show it’s working
-      var responseObj={
-        "fulfillmentText":response,
-        "fulfillmentMessages":[
-          {
-            "text": {
-              "text": [
-                "Hello vous demandé des informations sur le film " + movies
-              ]
-            }
+
+server.post('/', function(request,response) {
+  var occurence = request.body.queryResult.parameters.occurence
+  var responseObj = {}
+  if(occurence === 'conciergerie'){
+    responseObj={
+      "fulfillmentMessages":[
+        {
+          "text": {
+            "text": [
+              "Si j'ai bien comprit vous souhaitez faire une demande à la conciergerie, quelle est cette demande ?" 
+            ]
           }
-        ]
-        ,"source":""
-      } 
-    } else {
-      var responseObj={
-        "fulfillmentText":response,
-        "fulfillmentMessages":[
-          {
-            "text": {
-              "text": [
-                "Je ne comprends pas votre demaande"
-              ]
-            }
+        }
+      ]
+    } 
+  } else if(occurence === 'support') {
+    responseObj={
+      "fulfillmentMessages":[
+        {
+          "text": {
+            "text": [
+              "Si j'ai bien comprit vous souhaitez faire une demande au support, quelle est cette demande ?" 
+            ]
           }
-        ]
-        ,"source":""
-      } 
+        }
+      ]
     }
+  } else {
+    responseObj={
+      "fulfillmentMessages":[
+        {
+          "text": {
+            "text": [
+              "Je n'ai pas comprit votre demande, vous souhaitez faire une demande au support ou à la conciergerie ?" 
+            ]
+          }
+        }
+      ]
+    }
+  }
 
-    return res.json(responseObj);
+  return response.json(responseObj);
+})
 
-});
 server.get('/getName',function (req,res){
     res.send('Florian Fernandez');
-});
+})
 server.listen(port, function () {
     console.log("Server is up and running...");
-});
+})
+
+
+
+server.post('/getMoviestest',function (req,res)  {
+  var movies = req.body.queryResult.parameters.movies_name;
+  if(movies){
+    var response = "This is a sample response from your webhook!";//Default response from the webhook to show it’s working
+    var responseObj={
+      "fulfillmentText":response,
+      "fulfillmentMessages":[
+        {
+          "text": {
+            "text": [
+              "Hello vous demandé des informations sur le film " + movies
+            ]
+          }
+        }
+      ]
+      ,"source":""
+    } 
+  } else {
+    var responseObj={
+      "fulfillmentText":response,
+      "fulfillmentMessages":[
+        {
+          "text": {
+            "text": [
+              "Je ne comprends pas votre demaande"
+            ]
+          }
+        }
+      ]
+      ,"source":""
+    } 
+  }
+
+  return res.json(responseObj);
+
+})
