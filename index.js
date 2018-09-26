e9a1878f5108da5a5cca999c11e2b8f0eb7c05a8
fernandez <florian.fernandez@capgemini.com>
2018-09-26 17:23:00 +0200

    bla bla

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ server.post('/v2/gHook', function(request,response) {
   var requestUserContext = request.body.queryResult.parameters
   var requestUserQuery = request.body.queryResult.queryText
   var responseObj = {}
-  /*
+  
   if(requestUserContext.banque_subject){
     responseObj={
       "fulfillmentMessages":[
@@ -25,7 +25,7 @@ server.post('/v2/gHook', function(request,response) {
     } 
   } 
   return response.json(responseObj);
-  */
+
 })
 
 server.get('/getName',function (req,res){
