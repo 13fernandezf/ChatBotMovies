32f9d164cce1855252fffd613a2a2ea5fb2cbc13
fernandez <florian.fernandez@capgemini.com>
2018-09-27 17:41:37 +0200

    Coucou

--- index.js
+++ index.js
@@ -9,20 +9,60 @@ server.use(bodyParser.json());
 server.post('/v2/gHook', function(request,response) {
   var requestUserContext = request.body.queryResult.parameters
   var requestUserQuery = request.body.queryResult.queryText
+  var session = request.body.session;
   var responseObj = {}
   
   if(requestUserContext.banque_subject){
-    responseObj={
-      "fulfillmentMessages":[
-        {
-          "text": {
-            "text": [
-              "Très bien je transmets votre demande à l'équipe support. La demande est la suivante : '" + requestUserQuery + "'"
-            ]
-          }
+    responseObj=
+      {  
+        "fulfillmentText":"Coucou petite perruche 1",
+        "fulfillmentMessages":[  
+            {  
+                "text":[  
+                    "Coucou petite perruche 3"
+                ],
+      
+            }
+        ],
+        "source":"example.com",
+        "payload":{  
+            "google":{  
+                "expectUserResponse":true,
+                "richResponse":{  
+                    "items":[  
+                        {  
+                            "simpleResponse":{  
+                                "textToSpeech":"tCoucou petite perruche 2"
+                            }
+                        }
+                    ]
+                }
+            },
+            "facebook":{  
+                "text":"Hello, Facebook!"
+            },
+            "slack":{  
+                "text":"This is a text response for Slack."
+            }
+        },
+        "outputContexts":[  
+            {  
+                "name":session,
+                "lifespanCount":5,
+                "parameters":{  
+                    "param":"param value"
+                }
+            }
+        ],
+        "followupEventInput":{  
+            "name":"event name",
+            "languageCode":"en-FR",
+            "parameters":{  
+                "param":"param value"
+            }
         }
-      ]
-    } 
+      
+    }
   } 
   return response.json(responseObj);
 
@@ -34,4 +74,3 @@ server.get('/getName',function (req,res){
 server.listen(port, function () {
     console.log("Server is up and running...");
 })
-
